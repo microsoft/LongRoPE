12f69905d649c06692f520bdbb6d3c3a8bea816e
Yiyi-philosophy <yiyidematthew6@outlook.com>
2024-03-25 20:53:32 -0700

    la2 bf16

---
+++ script/ppl_eval/ppl_eval_proofpile_mis.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=1
+# export HF_DATASETS_CACHE="/path/to/store/model"
+export HF_DATASETS_CACHE="../cache"
+# path_dir=/your/path/to/store/model/or/dataset
+# your path to model
+
+source ./path_teamdrive.sh
+path_dir=$path_team
+
+
+# model setting
+declare -A setting
+
+# base llama2-7b 4k
+setting["base"]="--model ${path_dir}/Llama-2-7b-hf/ --method pi --factor 1.0"
+
+# together
+setting["together"]="--model ${path_dir}/LLaMA-2-7B-32K/ --method pi --factor 8.0"
+
+# longlora pi 100k 
+setting["longlora"]="--model ${path_dir}/Llama-2-7b-longlora-100k-ft/ --method pi --factor 25.0"
+
+# codellama 100k
+setting["codellama"]="--model ${path_dir}/CodeLlama-7b-hf/ --method dy_ntk --factor 1.0"
+
+# yarn 64k
+setting["yarn_64k"]="--model ${path_dir}/Yarn-Llama-2-7b-64k/ --method yarn --finetuned --factor 16.0"
+
+# yarn 128k
+setting["yarn_128k"]="--model ${path_dir}/Yarn-Llama-2-7b-128k/ --method yarn --finetuned --factor 32.0"
+
+# longrope 128k
+setting["longrope_128k"]="--model ${path_dir}/ft_out_model/cube-128k-dim-piece-mono-500-#m0/ck-400/ --method longrope --finetuned --factor 32.0"
+
+# longrope 256k
+setting["longrope_256k"]="--model ${path_dir}/ft_out_model/cube_256k_from_128k/ck-600/ --method longrope --finetuned --factor 64.0"
+
+
+# dataset setting
+# PROOFPILE_test="--tokenized ${path_team}/proofpile-test-tokenized --dataset_min_tokens 131072 --samples 1 --truncate"
+PG19_8k="--tokenized ${path_dir}/pg19-test-tokenized --samples 100 --sliding-window 256 "
+PROOFPILE_128k="--tokenized ${path_team}/proofpile-test-tokenized-mistral --dataset_min_tokens 131072 --samples 10 --truncate"
+PROOFPILE_256k="--tokenized ${path_team}/proofpile-test-tokenized-mistral --dataset_min_tokens 262144 --samples 10 --truncate"
+
+cache_dir="../cache_dir"
+output_dir=./evaluation/result
+
+save_memory="\
+--aggressive_mem_causal_lm"
+
+# save_memory="" # check
+
+# config_list=("base" "together" "longlora" "codellama" "yarn_64k" "yarn_128k" "longrope_128k" "longrope_256k")
+# config_list=("base") # check
+
+echo "dataset PROOFPILE 10sample"
+max_tokens_list=(4096 8192 32768 65536 98304 131072)
+# max_tokens_list=(4096 8192 16384 32768)
+
+echo $path_dir
+
+# step_list=("1_200" "1_300" "1_400" "1_500")
+step_list=("400")
+
+config_list=("longrope_128k_fp16") # check
+setting["longrope_128k_fp16"]="--model ${path_dir}/ft_out_model/cube-16k-mistral-128k/ck-400 --method longrope --finetuned --factor 32.0 --sliding_window_attention 131072"
+
+
+for step in "${step_list[@]}"; do
+    # setting["longrope_128k_bf16"]="--model ${path_dir}/ft_out_model/cube-mis-128k-bf16/ck-${step}/ --method longrope --finetuned --factor 32.0 --sliding_window_attention 131072"
+    max_tokens_list=(4096 8192 16384 32768 65536 98304 131072)
+    for config in "${config_list[@]}"; do
+        for max_tokens in "${max_tokens_list[@]}"; do
+            echo "####### step=$step, $config, max-tokens=$max_tokens #############"
+            python evaluation/perplexity.py \
+                ${PROOFPILE_128k}\
+                ${setting[$config]} \
+                --max_tokens $max_tokens \
+                --min_tokens $max_tokens \
+                --tokens_step 2048 \
+                --output_file "${output_dir}/compare_fp16_proofpile_${config}_${max_tokens}.csv" \
+                --original_max_position_embeddings 4096 \
+                --flash_attn \
+                ${save_memory} \
+                --cache_dir $cache_dir
+        done
+    done
+
+    # max_tokens_list=(262144)
+    # for config in "${config_list[@]}"; do
+    #     for max_tokens in "${max_tokens_list[@]}"; do
+    #         echo "####### step=$step, $config, max-tokens=$max_tokens #############"
+    #         python evaluation/perplexity.py \
+    #             ${PROOFPILE_256k}\
+    #             ${setting[$config]} \
+    #             --max_tokens $max_tokens \
+    #             --min_tokens $max_tokens \
+    #             --tokens_step 2048 \
+    #             --output_file "${output_dir}/compare_${step}_proofpile_${config}_${max_tokens}.csv" \
+    #             --original_max_position_embeddings 4096 \
+    #             --flash_attn \
+    #             ${save_memory} \
+    #             --cache_dir $cache_dir
+    #     done
+    # done
+done

8f067f29f0139368c9992b588f22a14d49a62ac1
jiahangxu <jiahangxu@microsoft.com>
2024-04-24 02:45:02 +0000

    auto merge + epoch

--- cube-new/cube-test/checkpoint/gen_test.sh
+++ cube-new/cube-test/checkpoint/gen_test.sh
@@ -5,6 +5,8 @@ for id in {0..3}; do
     touch checkpoint_1_200-shard${id}.pt
     touch checkpoint_2_300-shard${id}.pt
     touch checkpoint_4_500-shard${id}.pt
+    touch checkpoint1-shard${id}.pt
 done
 
 mv checkpoint_4_500-shard3.pt checkpoint_4_500-shard3.pt.tmp
+
